43e52204a4ed6d35baccb7c13e157ff65191ff28
Che4ter <che4ter@42.wtf>
2017-12-07 15:10:40 +0100

    add initial slide

--- examples.go
+++ examples.go
@@ -2,19 +2,3 @@ package main
 
 import "fmt"
 
-// fibonacci is a function that returns
-// a function that returns an int.
-func fibonacci() func() int {
-	x, y := 0, 1
-	return func() int {
-		x, y = y, x + y
-		return x
-	}
-}
-
-func calcfibonacci(number int) {
-	f := fibonacci()
-	for i := 0; i < number; i++ {
-		fmt.Println(f())
-	}
-}
--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
 
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	x, y := 0, 1
+	return func() int {
+		x, y = y, x + y
+		return x
+	}
+}
+
+func Calcfibonacci(number int) {
+	f := fibonacci()
+	for i := 0; i < number; i++ {
+		fmt.Println(f())
+	}
+}
+
 func main() {
-	calcfibonacci(20)
+	Calcfibonacci(9999)
 
 }
